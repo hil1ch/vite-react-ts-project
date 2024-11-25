6c9fa58dbac891a7f293deec6ab51d365418764b
Илья <ilya.panarin14@gmail.com>
2024-11-25 12:53:37 +0500

    Открытие окна подтверждения выхода

--- src/Widgets/Sidebar/SidebarNav/ExitButton.tsx
+++ src/Widgets/Sidebar/SidebarNav/ExitButton.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './ExitButton.css';
+import ApproveExit from '../../ApproveExit/ApproveExit';
 
 function ExitButton() {
+
+   const [isOpenExitModal, setIsOpenExitModal] = useState(false);
+
+   const openExitModal = () => {
+      setIsOpenExitModal(true);
+    };
+
    return (
-   <div className="sidebar__exit">
-      <img src="src\images\exit.svg" alt="" className="sidebar__img"></img>
-      <a className="sidebar__item-link" href="#">Выход</a>
-   </div>
-)}
+      <div className="sidebar__exit">
+         <img src="src\images\exit.svg" alt="" className="sidebar__img"></img>
+         <button type='button' className="sidebar__item-btn" onClick={openExitModal}>Выход</button>
+         {isOpenExitModal && <ApproveExit />}
+      </div>
+   )
+}
 
 export default ExitButton;
