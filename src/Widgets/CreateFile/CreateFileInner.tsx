df473aa58a9383f438710594d436d06db53f78af
Илья <ilya.panarin14@gmail.com>
2024-11-28 17:27:55 +0500

    Ограничение количества символов в заголовке и тексте

--- src/Widgets/CreateFile/CreateFileInner.tsx
+++ src/Widgets/CreateFile/CreateFileInner.tsx
@@ -1,13 +1,46 @@
+import { useState } from 'react';
 import './CreateFileInner.css';
 
+const MAX_FILE_TITLE_LENGTH = 20;
+const MAX_FILE_TEXT_LENGTH = 50;
+
 function CreateFileInner() {
+
+   const [fileTitle, setFileTitle] = useState<string>('');
+   const [fileText, setFileText] = useState<string>('');
+
+   const handleFileTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const titleValue = e.target.value;
+
+      if (titleValue.length <= MAX_FILE_TITLE_LENGTH) {
+         setFileTitle(titleValue);
+      }
+   }
+
+   const handleFileTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const textValue = e.target.value;
+
+      if (textValue.length <= MAX_FILE_TEXT_LENGTH) {
+         setFileText(textValue);
+      }
+   }
+
    return (
       <div className="file__wrapper">
          <div className="file__title">
-            <input className="input__file-title" type="text" placeholder="Заголовок" required></input>
+            <input className="input__file-title" 
+            type="text" 
+            placeholder="Заголовок" 
+            value={fileTitle}
+            onChange={handleFileTitleChange}
+            required></input>
             </div>
             <div className="file__text">
-               <textarea className="input__file-text" placeholder="Описание (до 50 символов)"></textarea>
+               <textarea className="input__file-text" 
+               placeholder="Описание (до 50 символов)"
+               value={fileText}
+               onChange={handleFileTextChange}>
+               </textarea>
             </div>
       </div>
    )
--- src/Widgets/Note/NoteInner.tsx
+++ src/Widgets/Note/NoteInner.tsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import './NoteInner.css';
 
+const MAX_NOTE_TITLE_LENGTH = 25;
+
 function NoteInner() {
+
+   const [noteTitle, setNoteTitle] = useState<string>('');
+
+   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const titleValue = e.target.value;
+
+      if (titleValue.length <= MAX_NOTE_TITLE_LENGTH) {
+         setNoteTitle(titleValue);
+      }
+   }
+
    return (
       <div className="note__wrapper">
          <div className="note__title">
-               <input className="input__note-title" type="text" placeholder="Заголовок" required></input>
+               <input className="input__note-title" 
+               type="text" 
+               placeholder="Заголовок" 
+               value={noteTitle}
+               onChange={handleTitleChange} 
+               required>
+               </input>
             </div>
             <div className="note__text">
                <textarea className="input__note-text" placeholder="Текст"></textarea>
