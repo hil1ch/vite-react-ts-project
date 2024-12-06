ab7a36fc67490eacd8dffb6ffca5b6f38339ee85
Илья <ilya.panarin14@gmail.com>
2024-12-06 19:26:47 +0500

    Изменение поля поиска по тегу

--- src/Shared/UI/SearchInput/SearchInput.tsx
+++ src/Shared/UI/SearchInput/SearchInput.tsx
@@ -3,7 +3,19 @@ import './SearchInput.css';
 function SearchInput() {
    return (
       <div>
-         <input type="search" placeholder='Поиск по тегу' className='search__input'/>
+         <form className='search__form'>
+            <label className="search__title" htmlFor="search__tags">Сортировка по тегу:</label>
+            <select className="tags__list" id="search__tags" name="search__tags">
+               <option value="Учеба">Все</option>
+               <option value="Учеба">Учеба</option>
+               <option value="Кулинария">Кулинария</option>
+               <option value="Здоровье">Здоровье</option>
+               <option value="Спорт">Спорт</option>
+               <option value="Цели">Цели</option>
+               <option value="Цели">Разработка</option>
+               <option value="Цели">Дизайн</option>
+            </select>
+         </form>
       </div>
    )
 }
--- src/Widgets/Note/NoteAddOptions.tsx
+++ src/Widgets/Note/NoteAddOptions.tsx
@@ -44,8 +44,8 @@ function NoteAddOptions() {
          <div className="add__tag">
             <img src="src\images\add-tag.svg" alt="" className="add__tag-img"></img>
             <form className='tag__form'>
-               <label className="tag__title" htmlFor="fruits">Выберите тег:</label>
-               <select className="tags__list" id="fruits" name="fruits" required>
+               <label className="tag__title" htmlFor="tags">Выберите тег:</label>
+               <select className="tags__list" id="tags" name="tags" required>
                   <option value="Учеба">Учеба</option>
                   <option value="Кулинария">Кулинария</option>
                   <option value="Здоровье">Здоровье</option>
