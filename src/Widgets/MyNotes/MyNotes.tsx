a870b4313c090b3a807ec4e7f01cc9018deff1ce
Илья <ilya.panarin14@gmail.com>
2024-12-21 21:28:26 +0500

    Получение моих заметок и их сортировка

--- src/Widgets/MyNotes/MyNotes.tsx
+++ src/Widgets/MyNotes/MyNotes.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import SearchInput from "../../Shared/UI/SearchInput/SearchInput";
 import MyPageNotes from "./MyPageNotes/MyPageNotes";
 import "./MyNotes.css";
 
 function MyNotes() {
+  const [selectedTag, setSelectedTag] = useState<string>("Все");
+  
+    const handleTagChange = (tag: string) => {
+      setSelectedTag(tag);
+    };
+
   return (
     <div className="my__page">
       <div className="my__page-inner">
-        <SearchInput />
-        <MyPageNotes />
+        <SearchInput onTagChange={handleTagChange}/>
+        <MyPageNotes selectedTag={selectedTag}/>
       </div>
     </div>
   );
--- src/Widgets/MyNotes/MyPageNotes/MyPageNotes.tsx
+++ src/Widgets/MyNotes/MyPageNotes/MyPageNotes.tsx
@@ -1,7 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
 import "./MyPageNotes.css";
 import PlaceholderImage from "../../../Shared/UI/PlaceholderNotePageImage/PlaceholderNotePageImage";
+import MainPageNote from "../../../Entities/MainPageNote/MainPageNote";
 
-function MyPageNotes() {
+interface Author {
+  email: string;
+  password: string;
+  activatedEmail: boolean;
+  registryCode: number;
+  gAcoount: boolean;
+  id: string;
+}
+
+interface MyNote {
+  description: string;
+  title: string;
+  author: Author;
+  noteTags: string[];
+}
+
+interface MyNotesTagProps {
+  selectedTag: string;
+}
+
+const fetchMyNotes = async (): Promise<MyNote[]> => {
+  const url = "http://localhost:5182/api/Note/GetAllNotesByUser";
+  const response = await fetch(url);
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw new Error(result.message || "Something went wrong");
+  }
+
+  return result;
+};
+
+function MyPageNotes({ selectedTag }: MyNotesTagProps) {
+  const {
+      data = [],
+      error,
+      isLoading,
+    } = useQuery<MyNote[], Error>({
+      queryKey: ['myNotes'],
+      queryFn: fetchMyNotes,
+    });
+  
+    if (isLoading) {
+      return <div>Загрузка...</div>;
+    }
+  
+    if (error instanceof Error) {
+      return <div>Error: {error.message}</div>;
+    }
+  
+    // Фильтруем заметки по тегу
+    const filteredMyNotes = selectedTag === "Все"
+      ? data
+      : data.filter(note => note.noteTags.includes(selectedTag));
+
   return (
     <div className="my__page-notes">
       <div className="my__notes-title__inner">
@@ -9,7 +65,13 @@ function MyPageNotes() {
       </div>
       <p className="my__notes-description">Заметки, созданные мной</p>
       <div className="my__page-notes-list">
-        <PlaceholderImage />
+      {filteredMyNotes.length > 0 ? (
+          filteredMyNotes.map((item, index) => (
+            <MainPageNote key={index} {...item} />
+          ))
+        ) : (
+          <PlaceholderImage />
+        )}
       </div>
     </div>
   );
