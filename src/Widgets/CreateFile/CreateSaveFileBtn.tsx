de4c19cfdd31a62dbb780b38581e2afde32cac31
Илья <ilya.panarin14@gmail.com>
2025-01-05 13:39:25 +0500

    Revert "Правки в CreateNote"
    This reverts commit c28adfef4a3443da0b0a0142e769a28a8f334e9f.

--- src/Widgets/CreateFile/CreateSaveFileBtn.tsx
+++ src/Widgets/CreateFile/CreateSaveFileBtn.tsx
@@ -1,12 +1,23 @@
 import { useCreateFile } from '../../Features/hooks/useCreateFile';
+import { useToggleFile } from '../../Features/hooks/useToggleFile';
 import './CreateSaveFileBtn.css';
 
-function CreateSaveFileBtn() {
+export interface IFile {
+   id: string;
+   done: boolean;
+}
+
+interface IFileActionsProps {
+   file: IFile
+}
+
+function CreateSaveFileBtn({file} : IFileActionsProps) {
    const {isPending} = useCreateFile();
+   const {toggleFile} = useToggleFile();
 
    return (
       <div className="file__action">
-         <button type="submit" className={`file__action-create-btn__save ${isPending ? "isDisabled" : ""}`} disabled={isPending}>
+         <button type="submit" className={`file__action-create-btn__save ${isPending ? "isDisabled" : ""}`} disabled={isPending} onChange={() => toggleFile(file.id, file.done)}>
             Сохранить
          </button>
       </div>
