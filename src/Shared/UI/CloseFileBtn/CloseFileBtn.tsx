05266d4e71fb7633782ee201bda2caf7aef754a1
Илья <ilya.panarin14@gmail.com>
2025-01-11 17:05:09 +0500

    Правки в создании заметки

--- src/Shared/UI/CloseFileBtn/CloseFileBtn.tsx
+++ src/Shared/UI/CloseFileBtn/CloseFileBtn.tsx
@@ -1,5 +1,4 @@
 import './CloseFileBtn.css';
-import ShareModal from '../ShareModal/ShareModal';
 
 interface ICloseFileBtnProps {
    closeModal?: () => void;
@@ -8,7 +7,6 @@ interface ICloseFileBtnProps {
 function CloseFileBtn({closeModal}: ICloseFileBtnProps) {
    return (
       <div className="close__file-btn">
-         <ShareModal />
          <button className="close__file-creating" onClick={closeModal}>
             <img src="src\images\close-file-creating.svg" alt="" className="close__file-creating__img"></img>
          </button>
