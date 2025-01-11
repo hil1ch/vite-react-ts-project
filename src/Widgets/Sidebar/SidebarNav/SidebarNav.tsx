78b9bf654cf9e4a058a4989fcb22e3370911a405
Илья <ilya.panarin14@gmail.com>
2025-01-11 17:15:19 +0500

    Правки в Sidebar

--- src/Widgets/Sidebar/SidebarNav/SidebarNav.tsx
+++ src/Widgets/Sidebar/SidebarNav/SidebarNav.tsx
@@ -1,12 +1,9 @@
 import { useModal } from '../../../Features/hooks/useModal';
 import MyNotes from './MyNotesLink';
-import NewFileButton from './NewFileButton';
 import NewNoteButton from './NewNoteButton';
-import OpenFiles from './OpenFilesLink';
-import OpenNotes from './OpenNotesLink';
 import MyFiles from './MyFilesLink';
 import Note from '../../Note/Note';
-import CreateFile from '../../CreateFile/CreateFile';
+
 import MyTodosLink from './MyTodosLink';
 import './SidebarNav.css';
 
@@ -16,17 +13,12 @@ function SidebarNav() {
 
    return (
       <div className="sidebar__nav">
-         <p className='sidebar__common'>Общее хранилище</p>
-         <OpenNotes />
-         <OpenFiles />
          <NewNoteButton onClick={() => openModal('note')}/>
-         <NewFileButton onClick={() => openModal('file')}/>
          <p className="sidebar__own">Личное хранилище</p>
          <MyTodosLink />
          <MyNotes />
          <MyFiles />
          {isOpenModal === 'note' && <Note closeModal={closeModal} />}
-         {isOpenModal === 'file' && <CreateFile closeModal={closeModal} />}
       </div>
    )
 }
